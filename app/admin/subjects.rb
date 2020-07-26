5fcea99d9a448e95e138decb946cae6b81501d0d
Jessy <1973008@maranatha.ac.id>
2020-07-27 00:01:05 +0700

    Note Application - 27/7/2020

---
+++ app/admin/subjects.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register Subject do
+
+  permit_params :name, note_ids: []
+
+  show title: 'My Subjects' do |variable|
+    h3 'You have ' + pluralize(subject.notes.count, 'notes') + ' for this subject'
+    subject.notes.each do |note|
+      div do 
+        h1 note.title
+        h4 link_to "[#{note.id}]: You created this note on #{note.created_at.strftime('%A,%d %B %Y at %l:%M %P')}", admin_note_path(note)
+      end
+    end
+  end
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
